0fce3dbe2e3347fbd41d66357e0a4db5ec89c9cb
al1-ce <al1-ce@null.net>
2024-11-01 13:08:30 +0300

    make keymap valid only if distro is despair

---
+++ ftplugin/c.lua
@@ -0,0 +1,16 @@
+vim.cmd([[
+    set omnifunc=cComplete#Complete
+    so $VIMRUNTIME/autoload/ccomplete.vim
+
+    setlocal path=.,,..,../..,./*,./*/*,../*,~/,~/**,/usr/include/*
+]])
+
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>gh",  "<cmd>call Switch_To_Header_Source()<cr>",     { desc = "[G]o [H]eader",          silent = true, noremap = true })
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>gsh", "<cmd>call Switch_To_Header_Source_SP()<cr>",  { desc = "[G]o [S]plit [H]eader",  silent = true, noremap = true })
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>gvh", "<cmd>call Switch_To_Header_Source_VSP()<cr>", { desc = "[G]o [V]split [H]eader", silent = true, noremap = true })
+if vim.g.distro == "despair.nvim" then
+    vim.keymap.set("n", "<leader>gh",  function() __switch_c_hc("none")   end, { desc = "[G]o [H]eader",          silent = true, noremap = true, buffer = true })
+    vim.keymap.set("n", "<leader>gsh", function() __switch_c_hc("split")  end, { desc = "[G]o [S]plit [H]eader",  silent = true, noremap = true, buffer = true })
+    vim.keymap.set("n", "<leader>gvh", function() __switch_c_hc("vsplit") end, { desc = "[G]o [V]split [H]eader", silent = true, noremap = true, buffer = true })
+end
+
